0089faa253c585fe29029ddc67a719858a113739
andytheelf <ballew22@gmail.com>
2020-05-17 18:29:42 -0500

    generateMarkdown addition

--- README.md
+++ README.md
@@ -13,4 +13,34 @@ none
 ## Licence(s)
 MIT
 ##Ask any questons atballew22@gmail.com
+undefined# ReadMe Generator
+This application was developed by: andytheelf
+
+Build a readme!
+## Installation
+Install by cloning repository! 
+## How to use the Application
+Install Inquirer Package
+## Issue Reporting and Contributing
+Reach out via email, or andytheelf gihub!
+## Other Contributing Developers:
+None
+## Licence(s)
+MIT
+##Ask any questons atballew22@gmail.com
+undefined# Andrea
+This application was developed by: Hello
+
+Stuff
+## Installation
+More stuff
+## How to use the Application
+ALL THE STUGG
+## Issue Reporting and Contributing
+yes
+## Other Contributing Developers:
+NONE
+## Licence(s)
+MIT
+##Ask any questons atballew22@gmail.com
 undefined
--- index.js
+++ index.js
@@ -1,80 +1,93 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-
-
-// array of questions 
-const questions = [
-    //name of the app 
-    {
-
-        type: 'input',
-        name: 'repoName',
-        message: 'What is name of this application? (This will aslo the main header of the README)',
-
-    },
-    //github user name 
-    {
-        type: 'input',
-        name: 'gitHubName',
-        message: 'What is your username on Github?',
-
-    },
-    //description of the project 
-    {
-        type: 'input',
-        message: 'Write a description of this application',
-        name: 'description',
-
-    },
-    // How do you install your app? 
-    {
-        type: 'input',
-        message: 'How does a user install your application? Provide instuctions here',
-        name: 'installApp',
-
-    },
-    // How does a user use your application? 
-    {
-        type: 'input',
-        message: 'How does a user use your application? Provide instuctions here',
-        name: 'appInstructions',
-
-    },
-    // report an issue, or make a contribution to it? 
-    {
-        type: 'input',
-        message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
-        name: 'appIssuesAndCont',
-
-    },
-    //What license they would you  like to use?  
-    {
-        type: 'input',
-        message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
-        name: 'license',
-
-    },
-    //Other Contributers 
-    {
-        type: 'input',
-        message: 'Who else contributed to your application? if none write "none" ',
-        name: 'contributors',
-
-    },
-    //Reach out with questions!
-    {
-        type: 'input',
-        message: 'If you have any questions about this README builder, please visit my github at andytheelf!',
-        name: 'githubLink',
-        url: 'https://github.com/andytheelf'
-
-    },
-
-];
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName) {
+    return Promise((resolve, reject) => {
+        fs.writeToFile('./Exmp/README.md', fileName, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        })
+    })
+}
+
+
+// array of questions 
+const questions = questionData => {
+    [
+        //name of the app 
+        {
+
+            type: 'input',
+            name: 'repoName',
+            message: 'What is name of this application? (This will aslo the main header of the README)',
+
+        },
+        //github user name 
+        {
+            type: 'input',
+            name: 'gitHubName',
+            message: 'What is your username on Github?',
+
+        },
+        //description of the project 
+        {
+            type: 'input',
+            message: 'Write a description of this application',
+            name: 'description',
+
+        },
+        // How do you install your app? 
+        {
+            type: 'input',
+            message: 'How does a user install your application? Provide instuctions here',
+            name: 'installApp',
+
+        },
+        // How does a user use your application? 
+        {
+            type: 'input',
+            message: 'How does a user use your application? Provide instuctions here',
+            name: 'appInstructions',
+
+        },
+        // report an issue, or make a contribution to it? 
+        {
+            type: 'input',
+            message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
+            name: 'appIssuesAndCont',
+
+        },
+        //What license they would you  like to use?  
+        {
+            type: 'input',
+            message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
+            name: 'license',
+
+        },
+        //Other Contributers 
+        {
+            type: 'input',
+            message: 'Who else contributed to your application? if none write "none" ',
+            name: 'contributors',
+
+        },
+        //Reach out with questions!
+        {
+            type: 'input',
+            message: 'If you have any questions about this README builder, please visit my github at andytheelf!',
+            name: 'githubLink',
+            url: 'https://github.com/andytheelf'
+
+        },
+
+    ]
+};
+
 
 // initialize program
 function init() {
@@ -174,4 +187,10 @@ function init() {
 
 
 // function call to initialize program
-init();
+init()
+    .then(questionData => {
+        return generateMarkdown(questionData);
+    })
+    .then(fileName => {
+        return writeToFile(fileName);
+    })
