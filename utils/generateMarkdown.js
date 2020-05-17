67dea362f54a37bf04979d66e1b96a9803ca01eb
andytheelf <ballew22@gmail.com>
2020-05-17 18:45:00 -0500

    function set up for genfunction script

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,20 @@
+//Install
+function createInstall(install) {}
+
+//Use function
+function createUsage(usage) {}
+
+//Contribution
+
+function createCont(contribution) {}
+
+//Testing
+
+function createTest(test) {}
+
+//License 
+function createLicense(license) {}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
